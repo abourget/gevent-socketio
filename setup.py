e07559c7edbd148160154ee1267ff10b725d605c
Jeffrey Gelens <jeffrey@gelens.org>
2011-05-11 23:05:08 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gevent-socketio",
-    version="0.2.0",
+    version="0.2.1",
     description="SocketIO server based on the Gevent pywsgi server, a Python network library",
     #long_description=open("README.rst").read(),
     author="Jeffrey Gelens",
--- socketio/__init__.py
+++ socketio/__init__.py
@@ -1,4 +1,4 @@
-version_info = (0, 2, 0)
+version_info = (0, 2, 1)
 __version__ = ".".join(map(str, version_info))
 
 __all__ = ['SocketIOServer']
