2385baa049979b344c6185ba2fb7b62c2e75f3bc
John Anderson <sontek@gmail.com>
2012-03-13 16:17:49 -0500

    added rooms support

--- socketio/namespace.py
+++ socketio/namespace.py
@@ -198,6 +198,8 @@ class BaseNamespace(object):
 
     def emit(self, event, data, broadcast=False, room=None,
              callback=None):
+        """ If room is set, it will broadcast """
+
         pkt = {
             'type': 'event',
             'name': event,
@@ -205,14 +207,16 @@ class BaseNamespace(object):
             'endpoint': self.ns_name
         }
 
-        encoded = packet.encode(pkt)
-        self.socket.put_client_msg(encoded)
+        self.socket.emit(self.ns_name, event, pkt, broadcast=broadcast,
+                room=room)
 
     def join(self, room):
-        pass
+        """ Joins the namespace to a specific room """
+        self.socket.join(self.ns_name, room)
 
     def leave(self, room):
-        pass
+        """ Removes the namespace from a specific room """
+        self.socket.leave(self.ns_name, room)
 
     def spawn(self, fn, *args, **kwargs):
         """Spawn a new process, attached to this Namespace.
@@ -229,10 +233,9 @@ class BaseNamespace(object):
     def kill_local_jobs(self):
         """Kills all the jobs spawned with BaseNamespace.spawn() on a namespace
         object.
-       
+
         This will be called automatically if the ``watcher`` process detects
         that the Socket was closed.
         """
         gevent.killall(self.jobs)
         self.jobs = []
-                    
