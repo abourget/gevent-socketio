b9b9537c40045fdda84a3e1bdeb49f0d8965f690
John Anderson <sontek@gmail.com>
2012-03-13 21:04:04 -0500

    refactored the code a little bit

--- socketio/__init__.py
+++ socketio/__init__.py
@@ -2,3 +2,29 @@ __version__ = (0, 2, 4)
 __all__ = ['SocketIOServer']
 
 from socketio.server import SocketIOServer
+
+import logging
+import gevent
+
+log = logging.getLogger(__name__)
+
+def socketio_manage(environ, namespaces, request=None):
+    """Main SocketIO management function, call from within your Framework of
+    choice's view.
+
+    The request object is not required, but will probably be useful to pass
+    framework-specific things into your Socket and Namespace functions.
+    """
+    socket = environ['socketio']
+    socket._set_environ(environ)
+    socket._set_namespaces(namespaces)
+    if request:
+        socket._set_request(request)
+    receiver_loop = socket._spawn_receiver_loop()
+    watcher = socket._spawn_watcher()
+
+    gevent.joinall([receiver_loop, watcher])
+
+    # TODO: double check, what happens to the WSGI request here ? it vanishes ?
+
+    return
