d79276bbd76d5b2684f51dc49bbe048cdd23c544
vv <vivek@fantain.com>
2012-06-29 21:12:15 +0530

    Fixed the policyserver code to handle proper HTTP response.  The code is from http://gehrcke.de/2011/06/the-best-and-simplest-tools-to-create-a-basic-websocket-application-with-flash-fallback-and-python-on-the-server-side/a

--- socketio/policyserver.py
+++ socketio/policyserver.py
@@ -1,9 +1,11 @@
 from gevent.server import StreamServer
+import socket
 
 __all__ = ['FlashPolicyServer']
 
 
 class FlashPolicyServer(StreamServer):
+    policyrequest = "<policy-file-request/>"
     policy = """<?xml version="1.0"?><!DOCTYPE cross-domain-policy SYSTEM "http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd">
 <cross-domain-policy><allow-access-from domain="*" to-ports="*"/></cross-domain-policy>"""
 
@@ -12,5 +14,14 @@ class FlashPolicyServer(StreamServer):
             listener = ('0.0.0.0', 843)
         StreamServer.__init__(self, listener=listener, backlog=backlog)
 
-    def handle(self, socket, address):
-        socket.sendall(self.policy)
+    def handle(self, sock, address):
+        # send and read functions should not wait longer than three seconds
+        sock.settimeout(3)
+        try:
+            # try to receive at most 128 bytes (`POLICYREQUEST` is shorter)
+            input = sock.recv(128)
+            if input.startswith(FlashPolicyServer.policyrequest):
+                sock.sendall(FlashPolicyServer.policy)
+        except socket.timeout:
+            pass
+        sock.close()
