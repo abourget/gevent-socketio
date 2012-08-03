14517a0fe4be394ca3e5014ee88a362a480b2956
GABRIEL DE LABACHELERIE <gdelabachelerie@ncegcolnx140.nce.amadeus.net>
2012-08-03 22:55:57 +0200

    Basic django integration

---
+++ socketio/sdjango.py
@@ -0,0 +1,27 @@
+import logging
+
+from socketio import socketio_manage
+from django.conf.urls import patterns, url, include
+from django.http import HttpResponse
+
+
+SOCKETIO_NS = {}
+
+class namespace(object):
+    def __init__(self, name=''):
+        self.name = name
+
+    def __call__(self, handler):
+        SOCKETIO_NS[self.name] = handler
+
+
+def socketio(request):
+    logging.getLogger("socketio").info(repr(SOCKETIO_NS))
+    try:
+        socketio_manage(request.environ, SOCKETIO_NS, request)
+    except:
+        logging.getLogger("socketio").error("Exception while handling socketio connection", exc_info=True)
+    return HttpResponse("")
+
+
+urls = patterns("", (r'', socketio))
