e99aa7ecb281675134efd0ff8a0cd9dfb53731b0
Aaron Fay <aaron.j.fay@gmail.com>
2012-09-19 12:21:35 -0600

    Increment rc version to rc3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ class PyTest(TestCommand):
 
 setup(
     name="gevent-socketio",
-    version="0.3.5-rc2",
+    version="0.3.5-rc3",
     description=(
         "SocketIO server based on the Gevent pywsgi server, "
         "a Python network library"),
