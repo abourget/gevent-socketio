668d11edbd62052cde1583be1e1d0512c930f16d
John Anderson <sontek@gmail.com>
2014-02-02 21:10:58 -0800

    Updating CHANGELOG for 0.3.6 release

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup
 from setuptools import find_packages
 from setuptools.command.test import test as TestCommand
 
+
 class PyTest(TestCommand):
     def finalize_options(self):
         TestCommand.finalize_options(self)
         self.test_args = []
         self.test_suite = True
+
     def run_tests(self):
         #import here, cause outside the eggs aren't loaded
         import pytest
