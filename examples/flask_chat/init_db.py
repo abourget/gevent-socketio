1965666634afd5273c0fa234c3302ea0dff3812f
shinriyo <shinriyo@gmail.com>
2015-01-05 19:20:17 +0900

    for prepend error
    If we don't run the program the code
    We encounter "KeyError in module 'threading'" error

--- examples/flask_chat/init_db.py
+++ examples/flask_chat/init_db.py
@@ -1,3 +1,10 @@
+import sys
+if 'threading' in sys.modules:
+    del sys.modules['threading']
+import gevent
+import gevent.socket
+import gevent.monkey
+gevent.monkey.patch_all()
+
 import chat
 chat.init_db()
-
